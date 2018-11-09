e05ab96d8602d7436d0b854264b276de86538ca7
Albina Zakirova <zak1804@gmail.com>
2018-11-09 03:52:11 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
   
 def create_an_array
-  my_array = ['orange', 'blue', 'yellow']
+  my_array = ['orange', 'blue', 'yellow', 'purple']
 end
 
 def add_element_to_end_of_array(my_array, element)
